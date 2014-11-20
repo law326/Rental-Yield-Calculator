fe4c4da48175b83d8d78f9502550385f0c879cd0
Henry Lo <henry.lo@monadsolutions.com>
2014-11-20 07:46:20 +0000

    add 'require net/http' for Windows OS

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
+require 'net/http' if ENV['OS'] == 'Windows_NT'
 
 # Initialize the Rails application.
 Rentify::Application.initialize!
